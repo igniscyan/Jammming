0c26477663edc94c74ccbcbcce59e8a931d26e52
boydDuffie <shmeeves@ufl.edu>
2020-05-05 22:10:58 -0400

    about to test initial rendering

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {SearchBar} from '../SearchBar/SearchBar';
 import {SearchResults} from '../SearchResults/SearchResults';
 import {Playlist} from '../Playlist/Playlist';
 
 
-function App() {
-  return (
-    <div>
-    <h1>Ja<span className="highlight">mmm</span>ing</h1>
-    <div className="App">
-      <!-- Add a SearchBar component -->
-      <div className="App-playlist">
-        <!-- Add a SearchResults component -->
-        <!-- Add a Playlist component -->
-      </div>
-    </div>
-  </div>
-}
+export class App extends React.Component() {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      "searchResults": []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,7 +4,7 @@ import './Track.css';
 export class Track extends React.Component {
     constructor(props)
     {
-        super(props)
+        super(props);
 
         this.state = {}
 
@@ -26,8 +26,8 @@ export class Track extends React.Component {
         return(
             <div className="Track">
                 <div className="Track-information">
-                    <h3><!-- track name will go here --></h3>
-                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {this.renderAction.bind(this)}
             </div>
